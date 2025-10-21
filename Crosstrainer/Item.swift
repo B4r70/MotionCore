0c9930e63994b5c6e2b0ce9ebdb664e425673063
Bartosz Stryjewski <bartosz@stryjewski.email>
2025-10-21 15:56:14 +0200

    Initial Commit

---
+++ Crosstrainer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Crosstrainer
+//
+//  Created by Barto on 21.10.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
